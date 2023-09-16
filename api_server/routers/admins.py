2e748f30018064adc00fc5e89266419ef1aca307
Hank <4A790162@stust.edu.tw>
2023-09-16 21:11:28 +0800

    Delete more than comments

--- api_server/routers/admins.py
+++ api_server/routers/admins.py
@@ -13,7 +13,6 @@ def get_admins_service() -> AdminServiceModule.AdminService:
 @router.get("/{admin_id}")
 async def read_admin(admin_id: str,admins_service: AdminServiceModule.AdminService = Depends(get_admins_service)):
     try:
-        # admins_service = get_admins_service()
         admin_data =await admins_service.fetch_admin(admin_id)
         if admin_data is None:
             raise HTTPException(status_code=404, detail="Data not found")
@@ -22,54 +21,36 @@ async def read_admin(admin_id: str,admins_service: AdminServiceModule.AdminServi
         raise he
     except Exception as e:
         # 開法者模式使用
-        return {"error": str(e)}
+        # return {"error": str(e)}
         # 這裡捕獲了任何其他的異常
-        # raise HTTPException(content={"message":"Server error"},status_code=500)
+        raise HTTPException(content={"message":"Server error"},status_code=500)
 
 @router.post("/")
 async def create_admin(admin: admins_model.AdminCreate,admins_service: AdminServiceModule.AdminService = Depends(get_admins_service)):
     try:
-        # admins_service = get_admins_service()
         await admins_service.create_admin(admin)
         return {"detail": "success"}
     except ValueError as ve:  # For data validation errors
         raise HTTPException(status_code=400, detail=str(ve))
     except Exception as e:
-        # 開法者模式使用
-        # return {"error": str(e)}
-        # raise HTTPException(content={"message":e},status_code=500)
-        # 這裡捕獲了任何其他的異常
-        raise HTTPException(detail={"detail": str(e)}, status_code=500)
-        # raise HTTPException(content={"message":"Server error"},status_code=500)
+        raise HTTPException(content={"message":"Server error"},status_code=500)
         
 @router.patch("/{admin_id}")
 async def update_admin(admin_id: str,admin: admins_model.AdminUpdate,admins_service: AdminServiceModule.AdminService = Depends(get_admins_service)):
     try:
-        # admins_service = get_admins_service()
         await admins_service.update_admin(admin_id,admin)
         return {"detail": "success"}
-    except ValueError as ve:  # For data validation errors
+    except ValueError as ve:  
         raise HTTPException(status_code=400, detail=str(ve))
     except Exception as e:
-        # 開法者模式使用
-        # return {"error": str(e)}
-        # raise HTTPException(content={"message":e},status_code=500)
-        # 這裡捕獲了任何其他的異常
         raise HTTPException(detail={"detail": str(e)}, status_code=500)
-        # raise HTTPException(content={"message":"Server error"},status_code=500)
         
 @router.delete("/{admin_id}")
 async def update_admin(admin_id: str,admins_service: AdminServiceModule.AdminService = Depends(get_admins_service)):
     try:
-        # admins_service = get_admins_service()
         await admins_service.delete_admin(admin_id)
         return {"detail": "success"}
-    except ValueError as ve:  # For data validation errors
+    except ValueError as ve:  
         raise HTTPException(status_code=400, detail=str(ve))
     except Exception as e:
-        # 開法者模式使用
-        # return {"error": str(e)}
-        # raise HTTPException(content={"message":e},status_code=500)
-        # 這裡捕獲了任何其他的異常
-        raise HTTPException(detail={"detail": str(e)}, status_code=500)
-        # raise HTTPException(content={"message":"Server error"},status_code=500)
+        raise HTTPException(content={"message":"Server error"},status_code=500)
