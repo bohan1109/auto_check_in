4e01ee479ad1fd1026b66e46210f9ee51457a2ec
Hank <4A790162@stust.edu.tw>
2023-09-18 23:01:52 +0800

    Confirm if the check in account exists

--- api_server/db/check_in_accounts.py
+++ api_server/db/check_in_accounts.py
@@ -13,6 +13,15 @@ class CheckInAccountDB:
             self.db = db_module.db
         else:
             self.db = db
+            
+    @staticmethod
+    def is_valid_objectid(id_str: str) -> bool:
+        try:
+            ObjectId(id_str)
+            return True
+        except InvalidId:
+            return False
+            
     async def create_check_in_account(self,check_in_account:CheckInAccountCreate):
         collection = self.db.check_in_accounts
         new_check_in_account = await collection.insert_one(check_in_account.dict())
@@ -20,3 +29,20 @@ class CheckInAccountDB:
             return check_in_account
         else:
             raise ValueError("Check in account could not be created")
+        
+    async def read_check_in_account_by_params(self, params: str, params_value: str):
+        query = {}
+        if params == "_id" and self.is_valid_objectid(params_value):
+            query[params] = ObjectId(params_value)
+        else:
+            query[params] = params_value
+
+        collection = self.db.check_in_accounts
+
+        admin_data = await collection.find_one(query)
+
+        if admin_data is None:
+            return None
+
+        admin_data["_id"] = str(admin_data["_id"])
+        return admin_data
--- api_server/services/check_in_accounts.py
+++ api_server/services/check_in_accounts.py
@@ -10,6 +10,9 @@ class CheckInAccountService:
         self._check_in_account_db=check_in_accounts_db
         
     async def create_check_in_account(self,check_in_account:check_in_accounts_models.CheckInAccountCreate):
+        have_check_in_account = await self._check_in_account_db.read_check_in_account_by_params("check_in_account", check_in_account.check_in_account)
+        if have_check_in_account:
+            raise ValueError("Check in account already exist")
         crawler_instance = CheckInCrawler()
         login_success = crawler_instance.login_result(check_in_account)
         if login_success:
