2018dc02761419a947ce2c921e4fc82e41af8d93
Hank <4A790162@stust.edu.tw>
2023-09-19 16:27:44 +0800

    Add jWT authentication to create_check_in routes

--- api_server/routers/check_in_accounts.py
+++ api_server/routers/check_in_accounts.py
@@ -1,24 +1,34 @@
 from fastapi import APIRouter
 from services import check_in_accounts as CheckInAccountServiceModule
-from fastapi import HTTPException,Depends
+from fastapi import HTTPException, Depends
 from models import check_in_accounts as check_in_accounts_model
 from fastapi.security import OAuth2PasswordBearer
+from utils.jwt_utils import get_current_admin
+from models import admins as admins_model
+
 router = APIRouter()
+
 
 def get_check_in_account_service() -> CheckInAccountServiceModule.CheckInAccountService:
     return CheckInAccountServiceModule.CheckInAccountService()
 
+
 @router.post("/")
-async def create_check_in_account(check_in_account: check_in_accounts_model.CheckInAccountCreate,check_in_accounts_service: CheckInAccountServiceModule.CheckInAccountService = Depends(get_check_in_account_service)):
+async def create_check_in_account(
+    check_in_account: check_in_accounts_model.CheckInAccountCreate,
+    check_in_accounts_service: CheckInAccountServiceModule.CheckInAccountService = Depends(get_check_in_account_service),
+    current_admin: admins_model.TokenData = Depends(get_current_admin)    
+):
     try:
-        
-        result = await check_in_accounts_service.create_check_in_account(check_in_account)
-        
+        result = await check_in_accounts_service.create_check_in_account(
+            check_in_account
+        )
+
         if result:
             return {"detail": "success"}
         else:
-            raise HTTPException(status_code=400,detail="Login fail")
+            raise HTTPException(status_code=400, detail="Login fail")
     except ValueError as ve:  # For data validation errors
         raise HTTPException(status_code=400, detail=str(ve))
     except Exception as e:
-        raise HTTPException(detail="Server error",status_code=500)
+        raise HTTPException(detail="Server error", status_code=500)
