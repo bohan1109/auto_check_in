2ef5a89f429e91a42f1e90ae4a21fce5b78c5404
Hank <4A790162@stust.edu.tw>
2023-09-22 00:00:47 +0800

    Add index route

--- api_server/main.py
+++ api_server/main.py
@@ -6,6 +6,9 @@ from routers import check_in_accounts as check_in_account_route
 import logging
 app = FastAPI()
 logging.basicConfig(level=logging.INFO)
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 app.include_router(admin_route.router, prefix="/admins", tags=["admins"])
 app.include_router(check_in_account_route.router, prefix="/check-in-accounts", tags=["check-in-accounts"])
