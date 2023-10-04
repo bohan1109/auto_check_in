8bbdb35655341e57dbe74c8c57e85058c2f89a90
Hank <4A790162@stust.edu.tw>
2023-10-04 20:17:33 +0800

    Add process time to logger

--- api_server/main.py
+++ api_server/main.py
@@ -4,7 +4,22 @@ from routers import admins as admin_route
 from routers import check_in_accounts as check_in_account_route
 
 import logging
-
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+import time
+from utils.process_time import ProcessTimeMiddleware
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+class ProcessTimeMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        start_time = time.time()
+        
+        response = await call_next(request)
+        
+        process_time = time.time() - start_time
+        response.headers["X-Process-Time"] = str(process_time)
+        logger.info(f"Processed request in {process_time:.4f} seconds")
+        return response
 app = FastAPI()
 logging.basicConfig(level=logging.INFO)
 @app.get("/api")
@@ -13,6 +28,8 @@ def read_root():
 
 app.include_router(admin_route.router, prefix="/api/admins", tags=["admins"])
 app.include_router(check_in_account_route.router, prefix="/api/check-in-accounts", tags=["check-in-accounts"])
+
+app.add_middleware(ProcessTimeMiddleware)
 
 @app.on_event("startup")
 async def startup_event():
