a2358cf35741d835f66541d8d66d39299ffee7ca
Hank <4A790162@stust.edu.tw>
2023-10-28 23:24:39 +0800

    Implement table component

---
+++ application_server/src/components/Table.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+
+interface DataTableProps {
+    rows: Array<any>;
+    columns: GridColDef[];
+    paginationModel: { page: number, pageSize: number };
+    pageSizeOptions: number[];
+}
+
+const DataTable: React.FC<DataTableProps> = ({ rows, columns, paginationModel, pageSizeOptions }) => {
+    return (
+        <div style={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                initialState={{
+                    pagination: {
+                        paginationModel: paginationModel,
+                    },
+                }}
+                pageSizeOptions={pageSizeOptions}
+                checkboxSelection
+            />
+        </div>
+    );
+}
+
+export default DataTable;
