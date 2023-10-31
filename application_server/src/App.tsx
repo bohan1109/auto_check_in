ea60d70c99fca54b1f3793ed6859ee297d5042a0
Hank <4A790162@stust.edu.tw>
2023-10-31 23:07:13 +0800

    Added jwtToken validation to routes

--- application_server/src/App.tsx
+++ application_server/src/App.tsx
@@ -6,17 +6,30 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Navigate
 } from "react-router-dom";
+type TokenContextType = {
+  tokenContext: string | null;
+  setTokenContext: React.Dispatch<React.SetStateAction<string | null>>;
+};
+
+export const TokenContext = React.createContext<TokenContextType | null>(null);
 
 const App: React.FC = () => {
+  const [tokenContext, setTokenContext] = React.useState(
+    localStorage.getItem("login_token")
+  );
+
   return (
+    <TokenContext.Provider value={{ tokenContext, setTokenContext }}>
       <Router>
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/check-in-account" element={<CheckInAccountFormPage />} />
+          <Route path="/home" element={!tokenContext?<Home />:<Navigate to="/" replace />} />
+          <Route path="/check-in-account" element={!tokenContext?<CheckInAccountFormPage />:<Navigate to="/" replace />} />
         </Routes>
       </Router>
+    </TokenContext.Provider>
   );
 }
 
