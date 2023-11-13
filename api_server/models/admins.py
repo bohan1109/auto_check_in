2906fc4ca962d8d1a96578b7a61e1cce54535dc3
Hank <4A790162@stust.edu.tw>
2023-11-13 22:21:25 +0800

    Add role field to admin model

--- api_server/models/admins.py
+++ api_server/models/admins.py
@@ -1,13 +1,20 @@
 from pydantic import BaseModel, validator
 from typing import Optional
+from enum import Enum
+
+class Role(str, Enum):
+    ADMIN = "admin"
+    USER = "user" 
+
 
 class AdminCreate(BaseModel):
     account:str
     password: str
     confirm_password: str
     username: str
+    role: Role
 
-    @validator("account", "username", "password", "confirm_password", pre=True)
+    @validator("account", "username", "password", "confirm_password","role", pre=True)
     def check_empty(cls, value):
         if value is None or str(value).strip() == "":
             raise ValueError("Data can not be empty")
@@ -23,6 +30,7 @@ class AdminUpdate(BaseModel):
     password: Optional[str] = None
     confirm_password: Optional[str] = None
     username: Optional[str] = None
+    role: Optional[Role] = None
     
     @validator("confirm_password", pre=True, always=True)
     def check_passwords_match(cls, confirm_password, values):
