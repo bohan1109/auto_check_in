898e3fac86787f6ea980282e664f2f78acb43e5d
Hank <4A790162@stust.edu.tw>
2023-11-16 18:14:54 +0800

    Implement navigate page button

--- application_server/src/components/AppBar.tsx
+++ application_server/src/components/AppBar.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-
+import { useNavigate } from 'react-router-dom';
 interface AppBarProps {
     title: string;
     onLogout: () => void;
     username:string
 }
 
+
+
 const CustomAppBar: React.FC<AppBarProps> = ({ title, onLogout,username }) => {
+    const navigate = useNavigate();
+    const navigateAdminPage = ()=>{
+        navigate('/admin')
+    }
+    const navigateHomePage = ()=>{
+        navigate('/home')
+    }
     return (
         <AppBar position="static">
             <Toolbar>
                 <Typography variant="h6" style={{ flexGrow: 1 }}>
                     {title}
                 </Typography>
+                <Button color="inherit" onClick={navigateHomePage}>首頁</Button>
+                <Button color="inherit" onClick={navigateAdminPage}>系統帳號列表</Button>
                 <Typography color="inherit">{username} 你好!</Typography>
                 <Button color="inherit" onClick={onLogout}>登出</Button>
             </Toolbar>
