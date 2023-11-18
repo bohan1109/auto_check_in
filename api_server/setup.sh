aac967e2053690289f0a76a899393e081c016797
Hank <4A790162@stust.edu.tw>
2023-11-18 20:40:17 +0800

    Update setup script for automated check-in and check-out

--- api_server/setup.sh
+++ api_server/setup.sh
@@ -27,8 +27,15 @@ echo "set-timezone Asia/Taipei"
 sudo docker image prune -a -f
 echo "remove idle images"
 
-(crontab -l 2>/dev/null | grep -q "35 8 \* \* \* docker exec api_server_web_1 python /usr/src/backend/check_in_script.py") || (crontab -l 2>/dev/null; echo "35 8 * * * docker exec api_server_web_1 python /usr/src/backend/check_in_script.py") | crontab -
-(crontab -l 2>/dev/null | grep -q "5 18 \* \* \* docker exec api_server_web_1 python /usr/src/backend/check_in_script.py") || (crontab -l 2>/dev/null; echo "5 18 * * * docker exec api_server_web_1 python /usr/src/backend/check_in_script.py") | crontab -
+# (crontab -l 2>/dev/null | grep -q "35 8 \* \* \* docker exec api_server_web_1 python /usr/src/backend/check_in_script.py") || (crontab -l 2>/dev/null; echo "35 8 * * * docker exec api_server_web_1 python /usr/src/backend/check_in_script.py") | crontab -
+for i in {30..55..5}; do
+    (crontab -l 2>/dev/null | grep -q "$i 8 * * * docker exec api_server_web_1 python /usr/src/backend/check_in_script.py") || (crontab -l 2>/dev/null; echo "$i 8 * * * docker exec api_server_web_1 python /usr/src/backend/check_in_script.py") | crontab -
+done
+(crontab -l 2>/dev/null | grep -q "00 9 \* \* \* docker exec api_server_web_1 python /usr/src/backend/check_in_script.py") || (crontab -l 2>/dev/null; echo "00 9 * * * docker exec api_server_web_1 python /usr/src/backend/check_in_script.py") | crontab -
+# (crontab -l 2>/dev/null | grep -q "5 18 \* \* \* docker exec api_server_web_1 python /usr/src/backend/check_in_script.py") || (crontab -l 2>/dev/null; echo "5 18 * * * docker exec api_server_web_1 python /usr/src/backend/check_in_script.py") | crontab -
+for i in {0..30..5}; do
+    (crontab -l 2>/dev/null | grep -q "$i 18 * * * docker exec api_server_web_1 python /usr/src/backend/check_out_account.py") || (crontab -l 2>/dev/null; echo "$i 18 * * * docker exec api_server_web_1 python /usr/src/backend/check_out_account.py") | crontab -
+done
 echo "set crontap"
 
 echo "Setup completed."
