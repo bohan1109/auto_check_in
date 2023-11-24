7031c20bb5c405a2ca94ac1a362448474776659c
Hank <4A790162@stust.edu.tw>
2023-11-24 15:55:20 +0800

    Add unit test for test_create_admin in AdminService

--- api_server/tests/services/test_admins.py
+++ api_server/tests/services/test_admins.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import AsyncMock, patch
 from services.admins import AdminService
+from models.admins import AdminCreate
 
 
 class TestAdminService(unittest.IsolatedAsyncioTestCase):
@@ -31,28 +32,55 @@ class TestAdminService(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(admins, mock_admins_data)
 
     async def test_fetch_admin_by_account(self):
-        mock_admins_data={
-                "_id": "some-fake-id-1",
-                "account": "admin_account",
-                "password": "hashedpassword",
-                "username": "Admin User",
-                "role": "admin",
+        mock_admins_data = {
+            "_id": "some-fake-id-1",
+            "account": "admin_account",
+            "password": "hashedpassword",
+            "username": "Admin User",
+            "role": "admin",
         }
         self.mock_db.read_admin_by_params = AsyncMock(return_value=mock_admins_data)
         admin = await self.admin_service.fetch_admin_by_account("admin_account")
         self.assertEqual(admin, mock_admins_data)
-        
+
     async def test_fetch_admin_by_id(self):
-        mock_admins_data={
-                "_id": "some-fake-id-1",
-                "account": "admin_account",
-                "password": "hashedpassword",
-                "username": "Admin User",
-                "role": "admin",
+        mock_admins_data = {
+            "_id": "some-fake-id-1",
+            "account": "admin_account",
+            "password": "hashedpassword",
+            "username": "Admin User",
+            "role": "admin",
         }
         self.mock_db.read_admin_by_params = AsyncMock(return_value=mock_admins_data)
         admin = await self.admin_service.fetch_admin_by_id("id")
         self.assertEqual(admin, mock_admins_data)
+
+    async def test_create_admin_account_exists(self):
+        mock_admin = AdminCreate(
+            account="existing_account",
+            password="password123",
+            confirm_password="password123",
+            username="username",
+            role="admin",
+        )
+        self.mock_db.read_admin_by_params = AsyncMock(return_value=True)
+        with self.assertRaises(ValueError):
+            await self.admin_service.create_admin(mock_admin)
+            
+    async def test_create_admin_account_success(self):
+        mock_admin = AdminCreate(
+            account="existing_account",
+            password="password123",
+            confirm_password="password123",
+            username="username",
+            role="admin",
+        )
+        
+        self.mock_db.read_admin_by_params = AsyncMock(return_value=None)
+        self.mock_db.create_admin = AsyncMock(return_value="admin_created")
+        result = await self.admin_service.create_admin(mock_admin)
+        self.assertEqual(result, "admin_created")
+
 
 if __name__ == "__main__":
     unittest.main()
