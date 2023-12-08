6da6a5fa9fdfc07a44ef06d788fe4116f6f7597e
Hank <4A790162@stust.edu.tw>
2023-12-08 11:58:54 +0800

    Add use_random_check_in field to create_check_in_account api

--- api_server/models/check_in_accounts.py
+++ api_server/models/check_in_accounts.py
@@ -9,8 +9,9 @@ class CheckInAccountCreate(BaseModel):
     owner:Optional[str] = None
     check_in_time:str
     check_out_time:str
+    use_random_check_in:bool
 
-    @validator("check_in_account", "check_in_password", "check_in_username","check_in_time","check_out_time", pre=True)
+    @validator("check_in_account", "check_in_password", "check_in_username","check_in_time","check_out_time","use_random_check_in", pre=True)
     def check_empty(cls, value):
         if value is None or str(value).strip() == "":
             raise ValueError("Data can not be empty")
